6fee24e2a0bb24babb3981b5d60df8386f5fe7a7
tomoki1639 <march.9.tomonaka@gmail.com>
2026-03-13 00:55:22 +0900

    feat: add timetable page

---
+++ src/app/timetable/page.tsx
@@ -0,0 +1,9 @@
+import TimetableView from "@/components/timetable/TimetableView";
+
+export default function TimetablePage() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <TimetableView />
+    </main>
+  );
+}
