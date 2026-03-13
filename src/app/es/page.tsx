86166f0be73f50ba56a4ff62bdcfe514c1ac1d3c
tomoki1639 <march.9.tomonaka@gmail.com>
2026-03-13 18:34:52 +0900

    fix: fix CalendarView syntax error

---
+++ src/app/es/page.tsx
@@ -0,0 +1,9 @@
+import ESManager from "@/components/es/ESManager";
+
+export default function ESPage() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <ESManager />
+    </main>
+  );
+}
