241319357d51bdfb21bdd87df41a03bfd9f7d551
tomoki1639 <march.9.tomonaka@gmail.com>
2026-03-13 22:39:35 +0900

    fix: fix prisma client setup

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "@prisma/adapter-pg/pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { Pool } = require("pg");
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// @ts-ignore
 const adapter = new PrismaPg(pool);
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
